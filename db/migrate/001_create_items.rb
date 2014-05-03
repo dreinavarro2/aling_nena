32e8641f393bfeedf9977bab34ced3ca4d45916f
Andrei Navarro <dreinavarro@gmail.com>
2014-05-03 22:11:14 +0800

    added bootstrap and admin section

--- db/migrate/001_create_items.rb
+++ db/migrate/001_create_items.rb
@@ -1,9 +1,10 @@
 class CreateItems < ActiveRecord::Migration
   def change
-    create_table :pages do |t|
+    create_table :items do |t|
       t.string :name
       t.decimal :price
       t.integer :quantity
+      t.integer :sold
     end
   end
 end
--- main.rb
+++ main.rb
@@ -1,6 +1,45 @@
 require 'sinatra'
 require './boot.rb'
 
+# ROUTES FOR ADMIN SECTION
 get '/admin' do
+  @products = Item.all
   erb :admin_index
 end
+
+get '/new_product' do
+  @product = Item.new
+  erb :product_form
+end
+
+post '/create_product' do
+  Item.create!(
+    name: params[:name],
+    price: params[:price],
+    quantity: params[:quantity],
+    sold: 0
+  )
+  redirect to '/admin'
+end
+
+get '/edit_product/:id' do
+  @product = Item.find(params[:id])
+  erb :product_form
+end
+
+post '/update_product/:id' do
+  @product = Item.find(params[:id])
+  @product.update_attributes!(
+    name: params[:name],
+    price: params[:price],
+    quantity: params[:quantity],
+  )
+  redirect to '/admin'
+end
+
+get '/delete_product/:id' do
+  @product = Item.find(params[:id])
+  @product.destroy!
+  redirect to '/admin'
+end
+# ROUTES FOR ADMIN SECTION
